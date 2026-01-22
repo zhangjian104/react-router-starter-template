fe31e0ff81ce1d5db9c5b0c4a0db2013df28699c
zhangjian17 <zhangjian17@kingsoft.com>
2026-01-22 17:30:19 +0800

    test

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -22,7 +22,7 @@ export function Welcome({ message }: { message: string }) {
 				<div className="max-w-[300px] w-full space-y-6 px-4">
 					<nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
 						<p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-							What&apos;s next?
+							你好，世界！
 						</p>
 						<ul>
 							{resources.map(({ href, text, icon }) => (
